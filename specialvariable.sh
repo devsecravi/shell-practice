828ef87cfa5db5f618dfa737c6a00e1579b37683
unknown <dasariravid145@gmail.com>
2026-02-20 13:49:07 +0530

    using varible to print conversations

--- specialvariable.sh
+++ specialvariable.sh
@@ -8,4 +8,6 @@ echo "Print which director you are in: $PWD"
 echo "which running the script: $USER"
 echo "who is running the script: $HOME"
 echo "pid of the script: $$"
+cho "pid of the running background: $!"
+echo "All argument passed to script: $*"
 sleep 10 &
