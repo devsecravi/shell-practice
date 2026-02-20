e4189ea2ff12950c10ba6486c8ec040ba3aba160
unknown <dasariravid145@gmail.com>
2026-02-20 12:32:36 +0530

    using varible to print conversations

---
+++ variabledec.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+preson1="ramesh"
+person2="rajesh"
+
+echo " hello $person1, How are you"
+echo " hello $person2, I am fine and what about you"
+echo " hello $person1, I am also fine, where are $person2"
+echo "i am in homw town $person2"
