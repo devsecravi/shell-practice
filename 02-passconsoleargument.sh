7d48c397459a835b46fcd87084c8631694942116
unknown <dasariravid145@gmail.com>
2026-02-21 08:54:49 +0530

    practining from first basic shell script

---
+++ 02-passconsoleargument.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "hello $person1, How are you"
+echo "hai $person2, I am fine and what about you"
+echo "i am also good, how is going on today $person1"
+echo "it's going on good $person2"
