1e96d9b4caf9be1f58f4e35f7b669cb60c474959
unknown <dasariravid145@gmail.com>
2026-02-21 09:27:31 +0530

    practining from first basic shell script

--- installpackage.sh
+++ installpackage.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-echo "installing"
+USERID=$(id -u)
+
+if [ $USERID ne 0 ]; then
+   echo "This command has to be run with superuser privileges"
+fi
 dnf install nginx -y
 echo "completed"
