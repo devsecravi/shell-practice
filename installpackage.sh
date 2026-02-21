3092371b6187a4afe9515f95d960b47f2def7d7d
unknown <dasariravid145@gmail.com>
2026-02-21 16:26:52 +0530

    commit the code for remove package shell script

--- installpackage.sh
+++ installpackage.sh
@@ -24,7 +24,7 @@ echo "Installing..."
 for pack in $@;
 do 
     dnf list installed $pack &>>$LOGS_FILE
-    if [ $? -ne 1 ]; then
+    if [ $? -ne 0 ]; then
       echo "Removing..."
       dnf remove $pack &>>$LOGS_FILE
       validate $? $pack "removing" | tee -a $LOGS_FILE
