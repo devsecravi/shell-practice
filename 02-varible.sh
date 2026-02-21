2b02324c0b7f4349ddba833cda0377117bd054bd
unknown <dasariravid145@gmail.com>
2026-02-21 08:51:30 +0530

    practining from first basic shell script

---
+++ 02-varible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ramesh
+person1=rajesh
+
+echo "Hello $person1, Good Morning"
+echo "Hi $person2, Very Good Morning"
+echo "$person1 what are doing now"
+echo "$person1 i have to go office "
