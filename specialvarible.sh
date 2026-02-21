061021953f95f1ca689b41466a7d149739e88dc4
unknown <dasariravid145@gmail.com>
2026-02-21 09:16:25 +0530

    practining from first basic shell script

---
+++ specialvarible.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Print the all arguments: $@"
+echo "Print the current shell file name:$0"
+echo "Print the current directory: $PWD"
+echo "print the USER NAME: $ID"
+echo "print the HOME Directry: $HOME"
+echo "Print the current root user: $USER"
+echo "print the previoes execute command: $?"
+echo "print the all types of argument: $*"
+echo "print current PID: $$"
