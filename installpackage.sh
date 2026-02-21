c6b542e6370aba16a85f69df2b93006f84025cdd
unknown <dasariravid145@gmail.com>
2026-02-21 13:49:19 +0530

    practining from first basic shell script

--- installpackage.sh
+++ installpackage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOGSFOLDER="/var/log/shell-script"
+LOGSFILE="/var/log/shell-script/$0.txt"
 
 if [ $USERID -ne 0 ]; then
    echo "This command has to be run with superuser privileges"
@@ -9,9 +11,9 @@ fi
 
 validate() {
      if [ $1 -ne 0 ]; then
-      echo "$2.....FAILURE"
+      echo "$2.....FAILURE" | &>> $LOGSFILE
       else
-      echo "$2....SUCESS"
+      echo "$2....SUCESS" | &>> $LOGSFILE
     fi
 }
  
@@ -22,9 +24,9 @@ do
 if [ $? -ne 0 ]; then
    echo "$package not installed, installing now"
    dnf install $package -y 
-   validate $? $package "Installing"
+   validate $? $package "Installing" | &>> $LOGSFILE
    else
-   echo "Already installed package: $package"
+   echo "Already installed package: $package" | &>> $LOGSFILE
 fi
 done
 
