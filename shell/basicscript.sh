513105fb0933c87dfdc01b0ab30f9ba3fe7792b9
unknown <dasariravid145@gmail.com>
2026-02-22 17:43:47 +0530

    action

--- shell/basicscript.sh
+++ shell/basicscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 number=23
-number2=45
+number2=34
 
 sumoftotal=$(($number+$number2))
 echo "Total Sum Of Value: $sumoftotal"
---
+++ shell/consolescript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+number1=$1
+number2=$2
+
+total=$((number1+number2))
+
+echo "total amount of: $total" 
