d9000adca76a722499dc58a5781f21f2aa7b2520
unknown <dasariravid145@gmail.com>
2026-02-22 18:54:47 +0530

    action

--- shell/condition.sh
+++ shell/condition.sh
@@ -3,7 +3,9 @@
 num=24
 
 if [ $num -eq 23 ]; then
-   echo "Given Number is equal: $num"
+   echo "Given Number is equal to: $num"
+elif [ $num -eq 24 ]; then
+    echo "Given Number is Equal to: $num"
 else
-    echo "Given Number is Not Equal: $num"
+    echo "Given Number is not Equal to: $num"
 fi
