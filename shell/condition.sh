ba2e946abab5d5a00e46ca06316099ebdab46ad2
unknown <dasariravid145@gmail.com>
2026-02-22 19:02:14 +0530

    action

--- shell/condition.sh
+++ shell/condition.sh
@@ -2,10 +2,17 @@
 
 num=25
 
+
 if [ $num -eq 23 ]; then
    echo "Given Number is equal to: $num"
 elif [ $num -eq 24 ]; then
     echo "Given Number is Equal to: $num"
 else
-    echo "Given Number is not Equal to: $num"
+    echo "Given Number is different : $num"
 fi
+
+
+for print in $num;
+do
+   echo "print upto $num: $print)"
+done
