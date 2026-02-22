8e55aec937c15626c11316f4efbd1aa194920cb4
unknown <dasariravid145@gmail.com>
2026-02-22 18:20:28 +0530

    action

--- shell/specialvariable.sh
+++ shell/specialvariable.sh
@@ -5,13 +5,21 @@ echo "All read Argument from Console: $@"
 echo "All read Argument from Console in single entity: $*"
 echo "All read Argument from Console count: $#"
 
-echo "Current ROOT USER: $USER"
-echo "Current HOME DIRECTORY: $HOME"
-echo "PRESENT WORK DIRECTORY: $PWD"
-echo "Shell Script Fetch File Name: $0"
+
 
 echo "Presnt PID: $$"
 sleep 10 &
 echo "PID backgroup process: $!"
+
+printmultiple(){
+    echo "Current ROOT USER: $USER"
+    echo "Current HOME DIRECTORY: $HOME"
+    echo "PRESENT WORK DIRECTORY: $PWD"
+    echo "Shell Script Fetch File Name: $0"
+}
+
+printmultiple 
+
+printmultiple
 
 
