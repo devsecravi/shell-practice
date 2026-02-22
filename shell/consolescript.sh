ea7c34fa8c4cab408c465a8bb5fdfdd1d16cb5da
unknown <dasariravid145@gmail.com>
2026-02-22 17:46:19 +0530

    action

--- shell/consolescript.sh
+++ shell/consolescript.sh
@@ -7,3 +7,11 @@ number2=$2
 total=$((number1+number2))
 
 echo "total amount of: $total" 
+
+echo "enter number one:"
+read number1
+echo "enter number two:"
+read number2
+
+totoalam=$(($number1+$number2))
+echo "total amount using read: $totoalam"
