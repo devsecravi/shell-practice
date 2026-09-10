2524e8a1e895231bf357a8b1799550fb37ba5298
unknown <dasariravid145@gmail.com>
2026-09-10 12:29:38 +0530

    shell script

--- 03-variable.sh
+++ 03-variable.sh
@@ -8,3 +8,7 @@ echo "The sum of ${NUM1} and ${NUM2} is ${SUM}"
 FRUITS=("apple" "banana" "cherry" "date")
 
 echo "The fruits in the array are: ${FRUITS[@]}"
+echo "The first fruit is: ${FRUITS[0]}"
+echo "The second fruit is: ${FRUITS[1]}"
+echo "The third fruit is: ${FRUITS[2]}"
+echo "The fourth fruit is: ${FRUITS[3]}"
