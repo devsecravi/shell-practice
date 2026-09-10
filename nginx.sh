40f7fbe1ee875ea5a66bd83abd0188cae7b4ee2d
unknown <dasariravid145@gmail.com>
2026-09-10 12:51:38 +0530

    shell script

--- nginx.sh
+++ nginx.sh
@@ -8,4 +8,10 @@ if [ $USERID -nq 0]; then
 fi
 
 echo "installing nginx web server"
-dnf install nginx -y
+dnf install nginxx -y
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install nginx"
+else
+    echo "nginx installed successfully"
+fi
