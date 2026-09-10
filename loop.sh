e68d5c89d2ad84cf71f7cea900643b4709643d49
unknown <dasariravid145@gmail.com>
2026-09-10 17:00:22 +0530

    shell script

---
+++ loop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+    echo "You are not root user, please run as root"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+validate(){
+      
+      if [ $1 -ne 0 ]; then
+             echo "$2 ....Filed" | tee -a $LOGS_FILE
+             exit 1
+        else
+             echo "$2 ....Success" | tee -a $LOGS_FILE
+        fi
+}
+
+for package in $@
+   
+do
+    dnf list installed$package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed, installing now" | tee -a $LOGS_FILE
+        dnf install $package -y &>>$LOGS_FILE
+        validate $? "Installing $package"
+    else
+         echo "$package is already installed" | tee -a $LOGS_FILE
+    fi
+done
