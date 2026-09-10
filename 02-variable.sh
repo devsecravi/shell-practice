2e532eaf79cc8908ff318a1924180b043cd52615
unknown <dasariravid145@gmail.com>
2026-09-10 12:08:24 +0530

    shell script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,3 +1,12 @@
-TIMESTAMP=$(date)
+STARTTIME=$(date +%s)
 
-echo "Current date and time is ${TIMESTAMP}"
+echo "Current date and time is ${STARTTIME}"
+
+sleep 10
+
+ENDTIME=$(date +%s)
+
+echo "end time is ${ENDTIME}"
+
+TOTALTIME=$(($ENDTIME - $STARTTIME))
+echo "Total time taken is ${TOTALTIME} seconds"
