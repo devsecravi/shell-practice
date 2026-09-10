5c3d5aa3707c5b3608005926f89dba631a4a2345
unknown <dasariravid145@gmail.com>
2026-09-10 12:28:34 +0530

    shell script

--- 03-variable.sh
+++ 03-variable.sh
@@ -4,3 +4,7 @@ NUM2=ravikumar
 SUM=$((NUM1 + NUM2))
 
 echo "The sum of ${NUM1} and ${NUM2} is ${SUM}"
+
+FRUITS=("apple" "banana" "cherry" "date")
+
+echo "The fruits in the array are: ${FRUITS[@]}"
