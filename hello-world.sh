52cb6959debd472a7bf530aa76ee1a1980bfbcc3
unknown <dasariravid145@gmail.com>
2026-09-10 11:34:52 +0530

    shell script

---
+++ hello-world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Suresh:: hello ramesh, how are you"
+echo "Ramesh:: I am fine, how are you"
+echo "Suresh:: I am also fine"
+echo "Ramesh:: Great, let's catch up soon"
+echo "Suresh:: Sure, let's plan for the weekend"
+echo "Ramesh:: Sounds good, see you then"
+echo "Suresh:: Bye, take care"
