23090c40ad929274c5e63fd0cdb748a368dccf6a
unknown <dasariravid145@gmail.com>
2026-09-10 11:51:06 +0530

    shell script

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}:: hello ${PERSON2}, how are you"
+echo "${PERSON2}:: hi ${PERSON1}, I am fine, how are you"
+echo "${PERSON1}:: I am also fine"
+echo "${PERSON2}:: Great, let's catch up soon"
+echo "${PERSON1}:: Sure, let's plan for the weekend"
+echo "${PERSON2}:: Sounds good, see you then"
+echo "${PERSON1}:: Bye, take care"
