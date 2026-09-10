fe9cd18f82b59c169b7f31f89fc8791b07000d22
unknown <dasariravid145@gmail.com>
2026-09-10 11:55:54 +0530

    shell script

---
+++ 01-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "plese enter the username"
+read USER_NAME
+echo "enter the username is ${USER_NAME}"
+echo "please enter the password"
+read PASSWORD
+echo "enter the password is ${PASSWORD}"
